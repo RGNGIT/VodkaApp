e95a4c61bb3ba2c020ccb26987cf7886aa56ba77
Nikita.Gnezdilov <Nikita.Gnezdilov@waveaccess.global>
2022-03-14 17:18:25 +0500

    +

--- VodkaApp/Main.Designer.cs
+++ VodkaApp/Main.Designer.cs
@@ -28,38 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listBoxLog = new System.Windows.Forms.ListBox();
             this.buttonStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labelVodkaAmount = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBoxLog
             // 
             this.listBoxLog.FormattingEnabled = true;
-            this.listBoxLog.Location = new System.Drawing.Point(272, 12);
+            this.listBoxLog.Location = new System.Drawing.Point(353, 11);
             this.listBoxLog.Name = "listBoxLog";
-            this.listBoxLog.Size = new System.Drawing.Size(516, 420);
+            this.listBoxLog.Size = new System.Drawing.Size(435, 420);
             this.listBoxLog.TabIndex = 0;
             // 
             // buttonStart
             // 
             this.buttonStart.Location = new System.Drawing.Point(5, 409);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(261, 23);
+            this.buttonStart.Size = new System.Drawing.Size(342, 23);
             this.buttonStart.TabIndex = 1;
             this.buttonStart.Text = "Запустить";
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // labelVodkaAmount
+            // 
+            this.labelVodkaAmount.AutoSize = true;
+            this.labelVodkaAmount.Location = new System.Drawing.Point(5, 390);
+            this.labelVodkaAmount.Name = "labelVodkaAmount";
+            this.labelVodkaAmount.Size = new System.Drawing.Size(0, 13);
+            this.labelVodkaAmount.TabIndex = 2;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(6, 376);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 13);
+            this.labelInfo.TabIndex = 3;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 443);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.labelVodkaAmount);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.listBoxLog);
             this.Name = "Main";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +95,9 @@
 
         private System.Windows.Forms.ListBox listBoxLog;
         private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label labelVodkaAmount;
+        private System.Windows.Forms.Label labelInfo;
     }
 }
 
--- VodkaApp/Main.cs
+++ VodkaApp/Main.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VodkaApp
@@ -17,9 +10,61 @@ namespace VodkaApp
             InitializeComponent();
         }
 
-        private void buttonStart_Click(object sender, EventArgs e)
+        int bottleAmount { get; set; } = 100;
+        float hours { get; set; } = 0.0f;
+        float timeUntilVodka { get; set; } = -1;
+
+        static int RndGen() => new Random().Next(1, 4);
+        void Logger(string message) => listBoxLog.Items.Add($"[Час {hours.ToString("0.0")}] {message}");
+
+        bool check = false;
+
+        new void Update()
+        {
+            labelVodkaAmount.Text = $"Бутылок осталось: {bottleAmount}. ";
+            DeliveryLogic();
+            IncomeLogic();
+        }
+
+        void DeliveryLogic()
+        {
+            if (bottleAmount < 50)
+            {
+                if (!check)
+                {
+                    timeUntilVodka = hours + 5f;
+                    check = true;
+                } else if(hours >= timeUntilVodka)
+                {
+                    bottleAmount += 100;
+                    check = false;
+                }
+                labelVodkaAmount.Text += $"У нас поставка через {Math.Abs(timeUntilVodka - hours).ToString("0.0")} часов.";
+            }
+        }
+
+        void IncomeLogic()
         {
 
+        }
+
+        void ShopIter(int amount)
+        {
+            Logger($"Покупатель взял {amount} бутылок водки.");
+            bottleAmount -= amount;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            buttonStart.Text = "Работаем...";
+            buttonStart.Enabled = false;
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ShopIter(RndGen());
+            hours += 0.1f;
+            Update();
         }
     }
 }
